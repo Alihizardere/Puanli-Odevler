51af5f3b8cca5538bd2a717a13d6fa5eb5abaacd
Ali Hızardere <aliduyguhzr@gmail.com>
2024-04-27 08:52:38 +0300

    initial commit

---
+++ BussTicketApp/BussTicketApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BussTicketApp
+//
+//  Created by alihizardere on 27.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Selamlar"
+    }
+
+
+}
+
